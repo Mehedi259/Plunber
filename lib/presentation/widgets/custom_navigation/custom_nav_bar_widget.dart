5f712fb668a0418abc2a3acb806ed1998fa57501
mehedihasanmridul <mmridul207@gmail.com>
2026-02-23 05:38:41 +0600

    navigation animation added

--- lib/presentation/widgets/custom_navigation/custom_nav_bar_widget.dart
+++ lib/presentation/widgets/custom_navigation/custom_nav_bar_widget.dart
@@ -17,10 +17,10 @@ class CustomNavBarWidget extends StatelessWidget {
   final Function(int) onItemSelected;
 
   const CustomNavBarWidget({
-    Key? key,
+    super.key,
     required this.selectedIndex,
     required this.onItemSelected,
-  }) : super(key: key);
+  });
 
   static const List<NavBarItem> _items = [
     NavBarItem(
@@ -59,7 +59,7 @@ class CustomNavBarWidget extends StatelessWidget {
         borderRadius: BorderRadius.circular(40),
         boxShadow: [
           BoxShadow(
-            color: const Color(0xFF2563EB).withOpacity(0.4),
+            color: const Color(0xFF2563EB).withValues(alpha: 0.4),
             blurRadius: 20,
             offset: const Offset(0, 8),
           ),
@@ -69,49 +69,146 @@ class CustomNavBarWidget extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: List.generate(_items.length, (index) {
           final isSelected = index == selectedIndex;
-          return GestureDetector(
+          return _NavBarItemWidget(
+            item: _items[index],
+            isSelected: isSelected,
             onTap: () => onItemSelected(index),
-            child: AnimatedContainer(
-              duration: const Duration(milliseconds: 300),
-              curve: Curves.easeInOut,
-              width: isSelected ? 84 : null,
-              height: isSelected ? 60 : null,
-              padding: EdgeInsets.symmetric(
-                horizontal: isSelected ? 8 : 12,
-                vertical: isSelected ? 6 : 8,
-              ),
-              decoration: BoxDecoration(
-                color: isSelected ? Colors.white : Colors.transparent,
-                borderRadius: BorderRadius.circular(30),
-              ),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    isSelected
-                        ? _items[index].activeImage
-                        : _items[index].inactiveImage,
-                    width: 24,
-                    height: 24,
-                    fit: BoxFit.contain,
-                  ),
-                  if (isSelected) ...[
-                    const SizedBox(height: 2),
-                    Text(
-                      _items[index].label,
-                      style: const TextStyle(
-                        color: Color(0xFF2563EB),
-                        fontWeight: FontWeight.w600,
-                        fontSize: 11,
-                      ),
-                    ),
-                  ],
-                ],
-              ),
-            ),
           );
         }),
+      ),
+    );
+  }
+}
+
+class _NavBarItemWidget extends StatefulWidget {
+  final NavBarItem item;
+  final bool isSelected;
+  final VoidCallback onTap;
+
+  const _NavBarItemWidget({
+    required this.item,
+    required this.isSelected,
+    required this.onTap,
+  });
+
+  @override
+  State<_NavBarItemWidget> createState() => _NavBarItemWidgetState();
+}
+
+class _NavBarItemWidgetState extends State<_NavBarItemWidget>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _scaleAnimation;
+  late Animation<double> _fadeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 300),
+      vsync: this,
+    );
+
+    _scaleAnimation = Tween<double>(
+      begin: 1.0,
+      end: 1.1,
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeInOut,
+      ),
+    );
+
+    _fadeAnimation = Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeInOut,
+      ),
+    );
+
+    if (widget.isSelected) {
+      _controller.forward();
+    }
+  }
+
+  @override
+  void didUpdateWidget(_NavBarItemWidget oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.isSelected != oldWidget.isSelected) {
+      if (widget.isSelected) {
+        _controller.forward();
+      } else {
+        _controller.reverse();
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: widget.onTap,
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+        width: widget.isSelected ? 84 : null,
+        height: widget.isSelected ? 60 : null,
+        padding: EdgeInsets.symmetric(
+          horizontal: widget.isSelected ? 8 : 12,
+          vertical: widget.isSelected ? 6 : 8,
+        ),
+        decoration: BoxDecoration(
+          color: widget.isSelected ? Colors.white : Colors.transparent,
+          borderRadius: BorderRadius.circular(30),
+          boxShadow: widget.isSelected
+              ? [
+                  BoxShadow(
+                    color: Colors.black.withValues(alpha: 0.1),
+                    blurRadius: 8,
+                    offset: const Offset(0, 2),
+                  ),
+                ]
+              : null,
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ScaleTransition(
+              scale: _scaleAnimation,
+              child: Image.asset(
+                widget.isSelected
+                    ? widget.item.activeImage
+                    : widget.item.inactiveImage,
+                width: 24,
+                height: 24,
+                fit: BoxFit.contain,
+              ),
+            ),
+            if (widget.isSelected) ...[
+              const SizedBox(height: 2),
+              FadeTransition(
+                opacity: _fadeAnimation,
+                child: Text(
+                  widget.item.label,
+                  style: const TextStyle(
+                    color: Color(0xFF2563EB),
+                    fontWeight: FontWeight.w600,
+                    fontSize: 11,
+                  ),
+                ),
+              ),
+            ],
+          ],
+        ),
       ),
     );
   }
